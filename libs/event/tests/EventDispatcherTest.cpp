6ecb930dbe830aab1d164dcc7b863b05622e86eb
Anton Wiklund <ixuz07@gmail.com>
2026-07-25 19:06:16 +0000

    test: ensure event dispatcher propagates exception if queue throws

--- libs/event/tests/EventDispatcherTest.cpp
+++ libs/event/tests/EventDispatcherTest.cpp
@@ -9,7 +9,7 @@ using antwika::event::EventDispatcher;
 using antwika::event::mocks::MockEventQueue;
 using antwika::event::mocks::MockEventSink;
 
-TEST(EventDispatcherTest, dispatch)
+TEST(EventDispatcherTest, Dispatch)
 {
     MockEventQueue mockEventQueue;
     MockEventSink mockEventSink;
@@ -20,4 +20,14 @@ TEST(EventDispatcherTest, dispatch)
     EXPECT_CALL(mockEventSink, handle(mockEvent)).Times(1);
 
     eventDispatcher.dispatch(mockEvent);
+}
+
+TEST(EventDispatcherTest, Dispatch_PropagatesExceptionWhenEventQueueEnqueueFails)
+{
+    MockEventQueue mockEventQueue;
+    EventDispatcher eventDispatcher(mockEventQueue, {});
+    Event mockEvent{.name = "mockEvent"};
+
+    EXPECT_CALL(mockEventQueue, enqueue(mockEvent)).WillOnce(::testing::Throw(std::runtime_error("mockException")));
+    EXPECT_THROW(eventDispatcher.dispatch(mockEvent), std::runtime_error);
 }
